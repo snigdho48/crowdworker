7400945f074dfe21ff9fc98c3ec9aac3a0f674e3
Atiquzzaman Snigdho <www.atik48@gmail.com>
2026-04-20 20:21:55 +0600

    update20/4

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = [
+    "https://crowdwork360.com",
+    "https://www.crowdwork360.com",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
